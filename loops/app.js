140e615bf65a0ddac577d0ba700ec7a798fb28a1
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-03 09:23:38 +0545

    sum of natural number

--- loops/app.js
+++ loops/app.js
@@ -1,1 +1,7 @@
-// loops 
+// loops reduce the repated code 
+
+
+for(let i=10;i>0;i--){
+    console.log(i);
+    
+}
---
+++ loops/sumofNnumber.js
@@ -0,0 +1,43 @@
+
+// sum of natural number  
+
+// natural number be 1,2,3,4,5,6,7,8,9,10
+
+function sumofn(){
+    let sum=0;
+ let i=0;
+
+for(i=1;i<=10;i++){
+    sum=sum+i;
+    
+}
+
+console.log(  sum);
+}
+ sumofn();
+
+
+
+
+// question 2
+
+// natural number be  10,9,8,7,6,5,4,3,2,1
+
+
+function sumofN(){
+
+let sum=0;
+
+let i=0;
+
+for(i=10;i>=1;i--){
+    sum=sum+i
+}
+console.log(sum);
+
+}
+
+sumofN()
+
+
+
