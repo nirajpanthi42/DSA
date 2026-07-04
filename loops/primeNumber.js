a366fd2c558fe11c06cd1483ce8a7ee1da72911f
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-04 17:18:04 +0545

    check primeNUmber

---
+++ loops/primeNumber.js
@@ -0,0 +1,27 @@
+
+//check prime number 
+
+//input:7
+//outout:true
+
+
+
+function checkprimeNumber(){
+  
+    let n=7
+
+     var prime=true
+     console.log(prime);
+     
+    for(let i=2;i<=Math.floor(n/2);i++){
+        if(n%i===0){
+          console.log(false)   
+        }
+      
+
+    }
+}
+
+checkprimeNumber()
+
+
