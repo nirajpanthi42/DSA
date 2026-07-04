073b38ec54ad71ebc333206df102a5b17cf05a4a
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-04 18:21:59 +0545

    reverseNumber

---
+++ loops/ReverseNumber.js
@@ -0,0 +1,25 @@
+
+// question:revers 123 
+//input:123
+//output:321
+
+
+
+
+function reverseNumber(n){
+
+    let rev=0;
+
+     while(n>0){        
+let rem=n%10;  
+   rev = rev * 10 + rem;
+n=Math.floor(n/10)     
+        
+     }
+     console.log(rev);
+     
+        
+}
+reverseNumber(123)
+
+ 
