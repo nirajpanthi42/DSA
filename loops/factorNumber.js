e7666dac9c409e641ac072d42d97ee0bb6569c01
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-04 11:27:07 +0545

    factors

---
+++ loops/factorNumber.js
@@ -0,0 +1,23 @@
+
+
+// find a the factor of a 12 
+//input :1,2,3,4,5,6,7,8,9,10
+//output:1,2,3,4,6
+
+
+
+
+function FactorNumber(){
+    let i=0;
+    for(i=1;i<=10;i++){
+      
+      if(12%i===0){
+   
+    console.log(i);
+        
+      }
+       
+    }
+}
+
+FactorNumber()
