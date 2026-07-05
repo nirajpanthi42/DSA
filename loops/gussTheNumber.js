25ba0df1810c31d286f7ce1f026c8a171ad48a40
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-05 21:10:40 +0545

    guess Number

---
+++ loops/gussTheNumber.js
@@ -0,0 +1,14 @@
+let n=12;
+ let guess=90;
+ 
+ while(guess!==n){
+     if(guss>1 || guss>100){
+         console.log("try again")
+         continue
+         if(guess>n)
+         
+         console.log("to high")
+     }else{
+         console.log("to low")
+     }
+ }
