69d878a1b25d9792069c8aaead4d46e2cef7e371
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-07 16:38:24 +0545

    rightangletraianglewithnumber

---
+++ patternProgramming/rightangletraianglewithnumber.js
@@ -0,0 +1,21 @@
+//print rightangleTraianglewithNumber
+
+//output:
+
+// 1
+// 12
+// 123
+// 1234
+// 12345 /
+
+
+
+for(let i=1;i<=5;i++){
+   for(let j=1;j<=i;j++){
+console.log(j);
+
+   
+   }
+    
+   console.log()
+}
