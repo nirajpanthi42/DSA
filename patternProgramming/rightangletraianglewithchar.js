ed3a0b0fd01638ab38528d516a1eb9091f5cda2b
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-07 16:56:31 +0545

    [200~rightangletraianglewithchar~

---
+++ patternProgramming/rightangletraianglewithchar.js
@@ -0,0 +1,32 @@
+
+//rightangletraianglewithnumber 
+
+//output
+
+//A
+//AB
+//ABC
+//ABCD
+//ABCDE / 
+
+
+
+
+for(let i=1;i<=5;i++){
+ 
+let assic=65
+
+    for(let j=1;j<=i;j++){
+
+       process.stdout.write(String.fromCodePoint(assic)+" ");
+      assic++
+       
+       
+    }
+
+    console.log();
+    
+
+}
+
+
