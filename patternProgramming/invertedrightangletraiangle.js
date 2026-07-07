21c1ba3233ccf087b91c3e552fad95dd44ae24cc
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-07 17:12:08 +0545

    [200~invertedrightangletraiangle~

---
+++ patternProgramming/invertedrightangletraiangle.js
@@ -0,0 +1,22 @@
+
+//print invertedrightangletrainagle 
+
+//ouput: 
+
+// *****
+// ****
+// ***
+// */
+
+
+
+
+
+
+for(let i=1;i<=5;i++){
+    for(let j=5;j<=i;j++){
+        process.stdout.write("*")
+    }
+    console.log();
+    
+}
