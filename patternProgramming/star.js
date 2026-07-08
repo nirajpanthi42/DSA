915c92e15133433daa2327a3d110cd629782d502
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-08 15:50:09 +0545

    starpattern

---
+++ patternProgramming/star.js
@@ -0,0 +1,39 @@
+
+// star
+//output:
+
+//*      *
+ // *  *  
+  //  *    
+  //*  *  
+//*      *
+
+
+
+
+
+
+
+for(let i=1;i<=10;i++){
+
+
+  
+
+
+
+    for(let j=1;j<=5;j++){
+       if(i==j || j+i==5+1){
+         process.stdout.write("*")
+       }else{
+        process.stdout.write("  ")
+        
+       }
+
+
+    }
+     
+
+
+    console.log();
+    
+}
