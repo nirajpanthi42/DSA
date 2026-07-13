5bb9f24977a4efc7b20fee61d3d85a796947fbaa
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-13 13:26:39 +0545

    arrayinputfromuser

--- Array/app.js
+++ Array/app.js
@@ -1,4 +1,8 @@
 
+const prompt=require("prompt-sync")();
+
+
+
 function helloWorld() {
    const message=["Hello","world!"];
   console.log(message.join(""));
@@ -6,3 +10,20 @@ function helloWorld() {
 }
 
 helloWorld();
+
+
+//collaction of data ;
+
+// linear data structure that stores a collection of elements in a contiguous block of memory .
+
+
+
+
+let arr=new Array(5);
+
+
+for(let i=0;i<arr.length;i++){
+    arr[i]=prompt("Enter the value of array at index"+i+":");
+}
+console.log(arr);
+
