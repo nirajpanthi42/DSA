e2cbbd4c5b71162f91c13e0b63625c7428bee8ea
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-16 08:38:49 +0545

    learnmaxelement

--- Array/maxelementfind.js
+++ Array/maxelementfind.js
@@ -1,16 +1,32 @@
+// question: find a max element in an array 
+//input: [2,3,4,6]
+//output:6
+
+
+
 
 
 let arr=[2,3,4,6]
 
+
 let max=arr[0];
 
+
 for(let i=1;i<arr.length;i++){
-    if(max>arr[i]){
+    if(max<arr[i]){
 max=arr[i];
     }
+
+
+
 }
 
 console.log(max);
+
+
+
+
+
 
 
 
