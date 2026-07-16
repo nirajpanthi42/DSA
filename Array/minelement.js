5fc248179e1735df40acc11bf4cd14f629f305ac
Niraj Panthi <nirajpanthi42@gmail.com>
2026-07-16 09:04:31 +0545

    learn minelement

---
+++ Array/minelement.js
@@ -0,0 +1,39 @@
+// question: find a min element in an array 
+//input: [2,3,4,6]
+//output:2
+
+
+
+
+
+let arr=[2,3,4,6]
+
+
+let min=arr[0];
+
+
+for(let i=1;i<arr.length;i++){
+    if(min>arr[i]){
+max=arr[i];
+    }
+
+
+
+}
+
+console.log(min);
+
+
+
+
+
+
+
+
+
+
+      
+   
+    
+
+
